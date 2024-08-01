33ba0a257974a91289936b81ab0a4c38155d7c30
Daniel Gilleran <dangilleran7@protonmail.com>
2024-07-31 21:43:10 -0400

    update

--- day_8/part_1/main.zig
+++ day_8/part_1/main.zig
@@ -3,14 +3,90 @@ const print = std.debug.print;
 
 pub fn main() !void {
     const buf = @embedFile("input.txt");
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+
+    var map = std.StringHashMap(Fork).init(alloc);
+    defer map.clearAndFree();
 
     var iter = std.mem.splitScalar(u8, buf, '\n');
 
+    var directions = Directions{ .string = iter.next().? };
+
     while (iter.next()) |line| {
         if (std.mem.eql(u8, line, "")) {
+            continue;
+        }
+        if (std.mem.eql(u8, line, "") and iter.peek() == null) {
             break;
         }
 
-        print("{s}\n", .{line});
+        var s_iter = std.mem.tokenizeAny(u8, line, " =(),");
+
+        const key = s_iter.next().?;
+        const left = s_iter.next().?;
+        const right = s_iter.next().?;
+        try map.put(key, Fork{ .left = left, .right = right });
+
+        // print("{s}\n", .{line});
     }
+
+    const goal = "ZZZ";
+    var curr_place: []const u8 = "AAA";
+    var steps: usize = 0;
+
+    while (!std.mem.eql(u8, curr_place, goal)) {
+        const dir = directions.next_dir();
+        const fork = map.get(curr_place).?;
+        // print("dir: {c}\n", .{dir});
+        // print("fork: {any}\n", .{fork});
+
+        if (dir == 'R') {
+            curr_place = fork.right;
+        } else {
+            curr_place = fork.left;
+        }
+        steps += 1;
+    }
+
+    print("Final Sum: {d}\n", .{steps});
+}
+
+const Directions = struct {
+    string: []const u8,
+    index: usize = 0,
+
+    pub fn next_dir(self: *Directions) u8 {
+        std.debug.assert(!(self.index > self.string.len));
+        if (self.index < self.string.len) {
+            const res = self.string[self.index];
+            // print("next_dir: {c}\n", .{res});
+            // print("index: {d}\n", .{self.index});
+            self.index += 1;
+            return res;
+        } else {
+            const res = self.string[0];
+            // print("next_dir: {c}\n", .{res});
+            // print("index: {d}\n", .{self.index});
+            self.index = 1;
+            return res;
+        }
+    }
+};
+
+const Fork = struct {
+    left: []const u8,
+    right: []const u8,
+};
+
+test {
+    var directions = Directions{ .string = "LLR" };
+
+    std.debug.assert(directions.next_dir() == 'L');
+    std.debug.assert(directions.next_dir() == 'L');
+    std.debug.assert(directions.next_dir() == 'R');
+    std.debug.assert(directions.next_dir() == 'L');
+    std.debug.assert(directions.next_dir() == 'L');
+    std.debug.assert(directions.next_dir() == 'R');
 }
---
+++ day_8/part_2/main.zig
@@ -0,0 +1,154 @@
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn main() !void {
+    const buf = @embedFile("input.txt");
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+
+    var dir_map = std.StringHashMap(Fork).init(alloc);
+    defer dir_map.clearAndFree();
+
+    var iter = std.mem.splitScalar(u8, buf, '\n');
+
+    var directions = Directions{ .string = iter.next().? };
+
+    // initial list of nodes ending with 'A'. Will be transformed into Forks
+    var node_list = std.ArrayList([]const u8).init(alloc);
+    defer node_list.clearAndFree();
+
+    while (iter.next()) |line| {
+        if (std.mem.eql(u8, line, "")) {
+            continue;
+        }
+        if (std.mem.eql(u8, line, "") and iter.peek() == null) {
+            break;
+        }
+
+        var s_iter = std.mem.tokenizeAny(u8, line, " =(),");
+
+        const key = s_iter.next().?;
+        if (std.mem.endsWith(u8, key, "A")) {
+            try node_list.append(key);
+        }
+        const left = s_iter.next().?;
+        const right = s_iter.next().?;
+        try dir_map.put(key, Fork{ .left = left, .right = right });
+
+        // print("{s}\n", .{line});
+    }
+
+    print("Node List Length: {d}\n", .{node_list.items.len});
+
+    // const goal = "ZZZ";
+    // var curr_place: []const u8 = "AAA";
+    var steps: usize = 0;
+    var all_ends_with_z: bool = false;
+    while (!all_ends_with_z) {
+        // const fork = dir_map.get(curr_place).?;
+        // print("dir: {c}\n", .{dir});
+        // print("fork: {any}\n", .{fork});
+        print("##Node List: {d}\n", .{steps});
+        for (node_list.items) |node| {
+            print("{s}\n", .{node});
+        }
+
+        all_ends_with_z = transform(&node_list, dir_map, directions.next_dir());
+
+        print("\n", .{});
+        steps += 1;
+        // if (steps == 1000) {
+        //     // std.debug.panic("FUCK", {});
+        //     std.posix.exit(1);
+        // }
+        // print("Steps: {d}\n", .{steps});
+    }
+
+    print("Final Sum: {d}\n", .{steps});
+}
+
+const Directions = struct {
+    string: []const u8,
+    index: usize = 0,
+
+    pub fn next_dir(self: *Directions) u8 {
+        std.debug.assert(!(self.index > self.string.len));
+        if (self.index < self.string.len) {
+            const res = self.string[self.index];
+            // print("next_dir: {c}\n", .{res});
+            // print("index: {d}\n", .{self.index});
+            self.index += 1;
+            return res;
+        } else {
+            const res = self.string[0];
+            // print("next_dir: {c}\n", .{res});
+            // print("index: {d}\n", .{self.index});
+            self.index = 1;
+            return res;
+        }
+    }
+};
+
+const Fork = struct {
+    left: []const u8,
+    right: []const u8,
+};
+
+// fn all_ends_with_z(arr: std.ArrayList([]const u8)) bool {
+//     for (arr.items) |node| {
+//         if (!std.mem.endsWith(u8, node, "Z")) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+fn transform(arr: *std.ArrayList([]const u8), dir_map: std.StringHashMap(Fork), dir: u8) bool {
+    var ends_with_z = true;
+    for (0..arr.items.len, arr.items) |i, node| {
+        const fork = dir_map.get(node).?;
+
+        var res: []const u8 = undefined;
+
+        if (dir == 'R') {
+            res = fork.right;
+        } else {
+            res = fork.left;
+        }
+
+        arr.*.items[i] = res;
+        if (!std.mem.endsWith(u8, res, "Z")) {
+            ends_with_z = false;
+        }
+    }
+    return ends_with_z;
+}
+test {
+    var directions = Directions{ .string = "LLR" };
+
+    std.debug.assert(directions.next_dir() == 'L');
+    std.debug.assert(directions.next_dir() == 'L');
+    std.debug.assert(directions.next_dir() == 'R');
+    std.debug.assert(directions.next_dir() == 'L');
+    std.debug.assert(directions.next_dir() == 'L');
+    std.debug.assert(directions.next_dir() == 'R');
+}
+test {
+    var directions = Directions{ .string = "RLRLRLLLRRRLLL" };
+
+    std.debug.assert(directions.next_dir() == 'R');
+    std.debug.assert(directions.next_dir() == 'L');
+    std.debug.assert(directions.next_dir() == 'R');
+    std.debug.assert(directions.next_dir() == 'L');
+    std.debug.assert(directions.next_dir() == 'R');
+    std.debug.assert(directions.next_dir() == 'L');
+    std.debug.assert(directions.next_dir() == 'L');
+    std.debug.assert(directions.next_dir() == 'L');
+    std.debug.assert(directions.next_dir() == 'R');
+    std.debug.assert(directions.next_dir() == 'R');
+    std.debug.assert(directions.next_dir() == 'R');
+    std.debug.assert(directions.next_dir() == 'L');
+    std.debug.assert(directions.next_dir() == 'L');
+    std.debug.assert(directions.next_dir() == 'L');
+}
