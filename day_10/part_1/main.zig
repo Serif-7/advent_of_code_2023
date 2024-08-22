a65a6a878523d5c7e81a16706360584e495e3343
Daniel Gilleran <dangilleran7@protonmail.com>
2024-08-21 22:54:36 -0400

    update

--- day_10/part_1/main.zig
+++ day_10/part_1/main.zig
@@ -7,15 +7,7 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const alloc = gpa.allocator();
 
-    var iter = std.mem.splitScalar(u8, buf, '\n');
-
-    while (iter.next()) |line| {
-        if (std.mem.eql(u8, line, "")) {
-            break;
-        }
-
-        print("{s}\n", .{line});
-    }
+    var grid = Grid.init(buf, alloc);
 }
 
 const Cursor = struct {
@@ -27,4 +19,29 @@ const Grid = struct {
     grid: std.ArrayList(std.ArrayList(u8)),
 
     fn get_adjacent_pos(self: Grid, coord: .{ usize, usize }) std.BoundedArray(.{ usize, usize }, 8) {}
+
+    fn init(string: []const u8, alloc: std.mem.Allocator) Grid {
+        var grid = std.ArrayList(std.ArrayList(u8)).init(alloc);
+
+        var iter = std.mem.splitScalar(u8, string, '\n');
+
+        for (0..iter.peek().len) |_| {
+            grid.append(std.ArrayList(u8).init(alloc));
+        }
+        while (iter.next()) |line| {
+            if (std.mem.eql(u8, line, "")) {
+                break;
+            }
+
+            //fill out grid
+            for (line, 0..line.len) |c, i| {
+                grid.items[i].append(c);
+            }
+            // print("{s}\n", .{line});
+        }
+
+        return Grid{
+            .grid = grid,
+        };
+    }
 };
